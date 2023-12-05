f00ac0bc05a472d348611da0aa28ffbb0caaa72f
Degisew <jegisew21@gmail.com>
2023-12-05 09:12:54 +0300

    get username and room name from query string

--- static/main.js
+++ static/main.js
@@ -4,6 +4,14 @@ const container = document.querySelector(".chat-messages");
 chat_form = document.getElementById("chat-form");
 const msg_input = document.getElementById("msg");
 
+// Get username and room name from URL query string.
+// I have added QS cdn instead of installing qs npm package.
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+})
+
+console.log(username, room);
+
 const outputMessage = (message) => {
   const div = document.createElement("div");
   div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
