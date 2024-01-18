5cf9724fe82ebe5a05a5f745e2f06f2b569172a1
Degisew <jegisew21@gmail.com>
2024-01-18 11:14:26 +0300

    Created a socket connection

--- socket.js
+++ socket.js
@@ -1,88 +1,42 @@
-const { message, chat, getMessages } = require("./utils/messages");
-const moment = require('moment');
-// const {
-//   userJoin,
-//   getCurrentUser,
-//   usersLeave,
-//   getRoomUsers,
-// } = require("./utils/users");
+// const { saveChat } = require("./utils/chat");
+const getPreviousMessages = require("./utils/messages");
+const createChatId = require("./utils/createChatRoom");
+// const moment = require("moment");
 
-const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
+// const timestamp = moment().format("YYYY-MM-DD HH:mm:ss");
+const timestamp = new Date().toLocaleString();
 
-console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', timestamp);
 const chatSocket = (io) => {
   io.on("connection", (socket) => {
+    console.log(`User is connected.${socket.id} `);
     try {
-      console.log("User is connected.");
-      // io.emit("start", dbMessages);
-      socket.on("chatMessage", ({chatId, messageId, text, sender }) => {
-        message(messageId, text, sender);
-        chat(chatId, messageId, text, sender, timestamp);
-        io.emit("message", { chatId: messageId, text, sender, time: timestamp});
-      });
-      socket.on("disconnect", () => {
-        console.log("User is left the chat.");
-      });
+      const chatId = createChatId(sender, receiver);
+
+      const loadPreviousMessages = async () => {
+        // Load previous messages from the database
+        const previousMessages = await getPreviousMessages(chatId);
+
+        // Emit previous messages to the connecting
+        socket.emit("previousMessages", previousMessages);
+      };
+      loadPreviousMessages();
     } catch (error) {
-      // Handle errors
-      console.error('Error in chatSocket:', error);
+      console.log("chat id", error);
     }
+
+    socket.on("chatMessage", ({ sender, receiver, text, chatIdN }) => {
+      io.emit("message", {
+        sender,
+        receiver,
+        text,
+        timestamp,
+        chatIdN,
+      });
+    });
+    socket.on("disconnect", () => {
+      console.log("User has left the chat.");
+    });
   });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const chatSocket = (io) => {
-//   io.on("connection", (socket) => {
-//     // socket.on("joinRoom", ({ username, room }) => {
-//     //   const user = userJoin(socket.id, username, room);
-//     //   socket.join(user.room);
-//       socket.broadcast
-//         .to(user.room)
-//         .emit(
-//           "message",
-//           messageFormat(chatBotName, `${user.username} has joined the chat.`)
-//         );
-//       // Send use and room info
-//       io.to(user.room).emit("roomUsers", {
-//         room: user.room,
-//         users: getRoomUsers(user.room),
-//       });
-//     });
-
-//     socket.on("chatMessage", (msg) => {
-//       const user = getCurrentUser(socket.id);
-//       io.to(user.room).emit("message", messageFormat(user.username, msg));
-//     });
-
-//     socket.on("disconnect", () => {
-//       const user = usersLeave(socket.id);
-//       if (user) {
-//         socket
-//           .to(user.room)
-//           .emit(
-//             "message",
-//             messageFormat(chatBotName, `${user.username} left the Chat room.`)
-//           );
-//         socket.leave(user.room);
-//       }
-//       // Send use and room info
-//       io.to(user.room).emit("roomUsers", {
-//         room: user.room,
-//         users: getRoomUsers(user.room),
-//       });
-//     });
-//   });
-// };
 
 module.exports = chatSocket;
