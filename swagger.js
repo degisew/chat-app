29d6d4349bc2cec3e05db12e94cff69046ee18ea
Degisew <jegisew21@gmail.com>
2024-01-18 11:15:00 +0300

    Added swagger docs

---
+++ swagger.js
@@ -0,0 +1,41 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Chat Application API",
+      version: "1.0.0",
+      description: "Documentation for the chat application API",
+      contact: {
+        email: "degisew.mengist21@gmail.com",
+      },
+    },
+    servers: [
+      {
+          // url: process.env.LOCAL_HOST,
+        url: process.env.chat,
+      },
+    ],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerOptions = swaggerJsdoc(options);
+
+module.exports = swaggerOptions;
