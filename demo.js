a112d30b8bda720de237c75aabc8b66d6342b342
Degisew <jegisew21@gmail.com>
2024-01-18 11:19:15 +0300

    Renamed filename

--- demo.js
+++
@@ -1,8 +0,0 @@
-const generateChatRoomId = ( receiverId, senderId) => {
-    return senderId < receiverId ? `${senderId}_${receiverId}` : `${receiverId}_${senderId}`;
-  };
-  
-  const chatRoomId = generateChatRoomId('@Dagi_spider', '@Meshueamer');
-
-  
-  console.log(chatRoomId);
